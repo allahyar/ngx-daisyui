4f0a32062857998c48d9bb548ec719407e2e1ca5
Allahyar <imAllahyar@gmail.com>
2022-09-11 15:50:27 +0430

    feat: add dialog module

---
+++ projects/daisy-ui/src/lib/components/dialog/dialog.service.ts
@@ -0,0 +1,66 @@
+import {
+	ApplicationRef,
+	ComponentFactoryResolver,
+	EmbeddedViewRef,
+	Injectable,
+	Injector,
+	StaticProvider,
+} from '@angular/core';
+import {ComponentType} from '@angular/cdk/overlay';
+import {DialogRef} from "./dialog-ref";
+import {delay, filter, of} from "rxjs";
+import {IDialogParams} from "@daisy/core";
+import {NavigationStart, Router} from "@angular/router";
+
+@Injectable()
+export class DialogService {
+
+	private dialogRef!: DialogRef<any, any>
+
+	constructor(private resolver: ComponentFactoryResolver,
+				private appRef: ApplicationRef,
+				private router: Router,
+				private injector: Injector
+	) {
+		this.closeOnRouteChanged()
+	}
+
+	open<TComponent, TResult>(component: ComponentType<TComponent>,
+							  options: Partial<IDialogParams>): DialogRef<TComponent, TResult> {
+
+		let factory = this.resolver.resolveComponentFactory(component);
+		this.dialogRef = new DialogRef<TComponent, TResult>(options);
+
+		const providers: StaticProvider[] = [
+			{provide: DialogRef, useValue: this.dialogRef}
+		];
+
+		const injector = Injector.create({providers, parent: this.injector});
+		const componentRef = factory.create(injector);
+
+
+		of(componentRef).pipe(delay(500)).subscribe(_ => {
+			componentRef.location.nativeElement.children[0].classList.add('modal-open')
+		})
+
+		document.body.classList.add('overflow-hidden');
+		this.appRef.attachView(componentRef.hostView)
+
+		let domElement = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+
+		(options.context as any && document.body).appendChild(domElement)
+
+		this.dialogRef.componentRef = componentRef;
+
+		return this.dialogRef;
+	}
+
+
+	closeOnRouteChanged() {
+		this.router.events
+			.pipe(filter(event => event instanceof NavigationStart))
+			.subscribe(() => {
+				this.dialogRef.close()
+			})
+	}
+}
