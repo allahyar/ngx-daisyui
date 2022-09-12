847f2f491420d81cf44e68278353ab63919b9905
Allahyar <imAllahyar@gmail.com>
2022-09-12 17:42:18 +0430

    wip: import some module into daisyui module

--- projects/daisy-ui/src/lib/daisy-ui.module.ts
+++ projects/daisy-ui/src/lib/daisy-ui.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
+import {DialogModule} from "./components/dialog/dialog.module";
+import {ButtonModule} from "./components/button/button.module";
+import {TextboxModule} from "./components/textbox/textbox.module";
+import {FormControlModule} from "./components/form-control/form-control.module";
 
 
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
+	  DialogModule,
+	  ButtonModule,
+	  TextboxModule,
+	  FormControlModule
   ],
   exports: [
+	  DialogModule,
+	  ButtonModule,
+	  TextboxModule,
+	  FormControlModule
   ]
 })
 export class DaisyUiModule { }
